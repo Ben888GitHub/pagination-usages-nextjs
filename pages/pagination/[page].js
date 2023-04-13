a706c45b3b8d658e0bf5f819ac00e9154b79244e
Benedict <benedictryan80@gmail.com>
2023-04-14 01:09:01 +0700

    pagination with ssg

--- pages/pagination/[page].js
+++ pages/pagination/[page].js
@@ -1,45 +1,45 @@
 // import Link from 'next/link';
 import ReactPaginate from 'react-paginate';
 import axios from 'axios';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
-const PaginationSSG = () => {
+const PaginationSSG = ({ data, page }) => {
 	// const { pageCount, posts, currentPage } = props;
 
-	// const router = useRouter();
+	const router = useRouter();
 
-	// const paginationHandler = (page) => {
-	// 	const currentPath = router.pathname;
-	// 	let currentQuery = router.query;
+	const paginationHandler = (page) => {
+		const currentPath = router.pathname;
+		let currentQuery = router.query;
 
-	// 	currentQuery.page = page.selected + 1;
-	// 	// console.log(currentQuery.page);
+		currentQuery.page = page.selected + 1;
+		// console.log(currentQuery.page);
 
-	// 	router.push({
-	// 		pathname: currentPath,
-	// 		query: currentQuery
-	// 	});
-	// };
+		router.push({
+			pathname: currentPath,
+			query: currentQuery
+		});
+	};
 
-	// data && console.log(data);
+	data && console.log(data);
 
 	return (
 		<div className="justify-center align-center mx-auto text-center mt-12">
 			<p className="text-4xl">Pagination with API</p>
-			{/* <ul className="m-10">
+			<ul className="m-10">
 				{data?.results?.map((post) => (
 					<li key={post.id}>{post.name}</li>
 				))}
-			</ul> */}
-			{/* <ReactPaginate
+			</ul>
+			<ReactPaginate
 				containerClassName="inline-flex -space-x-px"
 				pageClassName="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white "
 				activeClassName="px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
 				onPageChange={(event) => {
 					paginationHandler(event);
-					// console.log(event);
+					console.log(event);
 				}}
-				pageCount={pageCount}
+				pageCount={data.info.pages}
 				breakLabel={
 					<p className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
 						...
@@ -60,8 +60,8 @@ const PaginationSSG = () => {
 				}
 				pageRangeDisplayed={2} // todo, if it's mobile then 1 otherwise 2
 				marginPagesDisplayed={1}
-				initialPage={currentPage - 1}
-			/> */}
+				initialPage={page - 1}
+			/>
 		</div>
 	);
 };
@@ -71,23 +71,17 @@ export default PaginationSSG;
 export const getStaticProps = async ({ params }) => {
 	const { page } = params;
 
-	const url = 'https://ipapi.co/json/';
+	const { data } = await axios.get(
+		`https://rickandmortyapi.com/api/character?page=${page}`
+	);
 
-	// const { data } = await axios.get(
-	// 	`https://rickandmortyapi.com/api/character?page=${page}`
-	// );
-
-	// const { data } = await axios.get(url);
-	// console.log(data);
-	// console.log(page);
-	// console.log(data.results);
+	console.log(page);
+	console.log(data.results);
 
 	return {
 		props: {
-			// pageCount: pageCount && pageCount,
-			// posts: data?.results,
-			// currentPage: page
-			// data
+			page,
+			data
 		}
 	};
 };
