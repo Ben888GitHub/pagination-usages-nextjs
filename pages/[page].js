f3438bf9adef002f29e4df96f210e55f3c6331c1
Benedict <benedictryan80@gmail.com>
2023-04-13 22:50:16 +0700

    let see again

---
+++ pages/[page].js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const PageSSG = () => {
+	return (
+		<div className="justify-center align-center mx-auto text-center mt-12">
+			<p className="text-4xl">Page SSG</p>
+		</div>
+	);
+};
+
+export default PageSSG;
+
+export const getStaticProps = async (context) => {
+	return {
+		props: {
+			data: 'hi'
+		}
+	};
+};
+
+export const getStaticPaths = async () => {
+	return {
+		paths: [],
+		fallback: 'blocking'
+	};
+};
